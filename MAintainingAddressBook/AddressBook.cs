75319ae1c54e8adeeab0538c61beaacabc7b726a
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-20 16:32:49 +0530

    [VINAY] Add:Displaying after Editing Data

--- MAintainingAddressBook/AddressBook.cs
+++ MAintainingAddressBook/AddressBook.cs
@@ -11,7 +11,8 @@ namespace MaintainingAddressBook
         List<Contact> addressBook = new List<Contact>();
         public AddressBook()
         {
-            Contact address1 = new Contact()
+            Console.WriteLine("Enter the Data");
+            Contact address = new Contact()
             {
                 FirstName = Console.ReadLine(),
                 LastName = Console.ReadLine(),
@@ -21,20 +22,8 @@ namespace MaintainingAddressBook
                 EmailAddress = Console.ReadLine(),
                 PostalCode = Convert.ToInt32(Console.ReadLine()),
                 MobileNumber = Convert.ToInt64(Console.ReadLine())
-            };
-            Contact address2 = new Contact()
-            {
-                FirstName = Console.ReadLine(),
-                LastName = Console.ReadLine(),
-                Address = Console.ReadLine(),
-                City = Console.ReadLine(),
-                State = Console.ReadLine(),
-                EmailAddress = Console.ReadLine(),
-                PostalCode = Convert.ToInt32(Console.ReadLine()),
-                MobileNumber = Convert.ToInt64(Console.ReadLine())
-            };
-            addressBook.Add(address1);
-            addressBook.Add(address2);
+            };           
+            addressBook.Add(address);
         }
         public void AddContactToAddressBook(Contact contact)
         {
@@ -47,6 +36,46 @@ namespace MaintainingAddressBook
                 Console.WriteLine(contact.FirstName + " " + contact.LastName + " " + contact.Address +" "+ contact.City + " " + contact.State + " " + contact.EmailAddress + " " + " " + contact.PostalCode + " " + contact.MobileNumber);
             }
         }
+        public void EditContactInAddressBook(string name)
+        {
+            foreach (var contact in addressBook)
+            {
+                if (contact.FirstName.Equals(name))
+                {
+                    Console.WriteLine("Enter the Option To Update");
+                    int option = Convert.ToInt32(Console.ReadLine ());
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter the FirstName to Update");
+                            contact.FirstName  = Console.ReadLine(); break;
+                        case 2:
+                            Console.WriteLine("Enter the LastName to Update");
+                            contact.LastName = Console.ReadLine(); break;
+                        case 3:
+                            Console.WriteLine("Enter the Address to Update");
+                            contact.Address = Console.ReadLine(); break;
+                        case 4:
+                            Console.WriteLine("Enter the City to Update");
+                            contact.City = Console.ReadLine(); break;
+                        case 5:
+                            Console.WriteLine("Enter the State to Update");
+                            contact.State = Console.ReadLine(); break;
+                        case 6:
+                            Console.WriteLine("Enter the Email to Update");
+                            contact.EmailAddress = Console.ReadLine(); break;
+                        case 7:
+                            Console.WriteLine("Enter the Postal to Update");
+                            contact.PostalCode = Convert.ToInt32(Console.ReadLine()); break;
+                        case 8:
+                            Console.WriteLine("Enter the MobileNumber to Update");
+                            contact.MobileNumber = Convert.ToInt64(Console.ReadLine()); break;
+                    }
+                }
+                Display();
+            }
+        }
+
 
     }
 }
--- MAintainingAddressBook/Program.cs
+++ MAintainingAddressBook/Program.cs
@@ -5,17 +5,25 @@ public class Program
     public static void Main(String[] args)
     {
         Console.WriteLine("********** Welcome in this Address Book ************");
-        Contact  addressBook = new Contact();
+        AddressBook addressBook = new AddressBook();
         Console.WriteLine("Please Enter the Information");
-        addressBook.FirstName = Console.ReadLine();
-        addressBook.LastName = Console.ReadLine();
-        addressBook.Address = Console.ReadLine();
-        addressBook.City= Console.ReadLine();
-        addressBook.State = Console.ReadLine();
-        addressBook.EmailAddress = Console.ReadLine();
-        addressBook.PostalCode = Convert.ToInt32(Console.ReadLine());
-        addressBook.MobileNumber = Convert.ToInt64(Console.ReadLine());
-        Console.WriteLine(addressBook.FirstName + " " + addressBook.LastName + " " + addressBook.Address +" "+ addressBook.City + " " + addressBook.State + " " + addressBook.EmailAddress + " " + addressBook.PostalCode + " " + addressBook.MobileNumber);
+        bool check = true;
+        Console.WriteLine(" Enter the Zero to Stop The Execution 0\n Display The Information1\n Edit the Information 2\n");
 
+        while (check)
+        {
+            Console.WriteLine("***** Please Enter The Above Information *******");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1: addressBook.Display(); break;
+                case 2:
+                    Console.WriteLine("Enter the Name for Edit the Information");
+                    string name = Console.ReadLine();
+                    addressBook.EditContactInAddressBook(name); break;
+                case 0: check = false; break;
+            }
+
+        }
     }
 }
