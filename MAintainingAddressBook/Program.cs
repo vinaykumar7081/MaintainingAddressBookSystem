2a98cd3f59fd56dda4fc599f1b560a6ec7cb929f
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-20 14:23:51 +0530

    [VINAY] Add: Displaying the One Employee Information

---
+++ MAintainingAddressBook/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MaintainingAddressBook;
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("********** Welcome in this Address Book ************");
+        Contact  addressBook = new Contact();
+        Console.WriteLine("Please Enter the Information");
+        addressBook.FirstName = Console.ReadLine();
+        addressBook.LastName = Console.ReadLine();
+        addressBook.Address = Console.ReadLine();
+        addressBook.City= Console.ReadLine();
+        addressBook.State = Console.ReadLine();
+        addressBook.EmailAddress = Console.ReadLine();
+        addressBook.PostalCode = Convert.ToInt32(Console.ReadLine());
+        addressBook.MobileNumber = Convert.ToInt64(Console.ReadLine());
+        Console.WriteLine(addressBook.FirstName + " " + addressBook.LastName + " " + addressBook.Address + " " + addressBook.State + " " + addressBook.EmailAddress + " " + addressBook.PostalCode + " " + addressBook.MobileNumber);
+
+    }
+}
